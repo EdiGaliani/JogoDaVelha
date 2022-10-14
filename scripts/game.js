97629700fd9b7debd85fcd9193c345404010ae6b
galiano <edigaliano@gmail.com>
2022-10-14 13:47:14 -0300

    Modifications with Fernando

--- scripts/game.js
+++ scripts/game.js
@@ -4,11 +4,11 @@ let board = ["","","","","","","","",""];
 let playerTime = 0;
 let symbols = ["o", "x"];
 
-function handleMove(position) {
+/* function handleMove(position) {
     board[position] = symbols[playerTime];
     if(playerTime == 0) {
         playerTime = 1;
     }else{
         playerTime = 0;
     }
-}
+} */
--- scripts/interface.js
+++ scripts/interface.js
@@ -1,14 +1,21 @@
 
 document.addEventListener('DOMContentLoaded', () => {
     let squares = document.querySelectorAll(".square");
-    squares.forEach((square) => {
-        square.addEventListener('click', handleClick);
+        squares.forEach((square, index) => {
+           /*  if(index % 2 === 0) {
+                square.style.backgroundColor = "black";
+            }else {
+                square.style.backgroundColor = "white";
+            } */
+            let isPar = index % 2 === 0;
+            square.style.backgroundColor = (isPar) ? "black" : "white";
+            square.addEventListener('click', handleClick);
     })
 })
 
 function handleClick(event) {
-    let square = event.target;
-    let position = square.id;
+    let squareClicked = event.target;
+    let position = squareClicked.id;
     handleMove(position);
     updateSquares();
 }
@@ -20,7 +27,7 @@ function updateSquares() {
         let symbol = board[position];
 
         if(symbol != "") {
-            square.innerHTML = `<div class='${symbol}'></div>`
+            square.innerHTML = `<div class='${symbol}'></div>`;
         }
     })
 }
